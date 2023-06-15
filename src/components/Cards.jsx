9304281105f2e1ba92a37fd2f9d7f519807e2dc7
Seryeong Lee <srlimvp@gmail.com>
2023-06-15 22:28:35 +0900

    수정부분의 컴포넌트 분리는 나중에 하고 먼저 버튼 클릭 시 이벤트 처리 먼저 하기

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,35 @@
 import Button from './Button';
-import UpdateContent from './UpdateContent';
+import { useEffect, useState } from 'react';
+// import UpdateContent from './UpdateContent';
 
 const Cards = ({item, fnc1, fnc2,fnc3,fnc4, c}) => {
+  const [newTitle, setnewTitle] = useState(item.work);
+    const [newContent, setnewContent] = useState(item.content);
+
+    const data = JSON.parse(localStorage.getItem("todokey"));
+
+    const newTitleHandler = (event) => {
+        setnewTitle(event.target.value);
+    };
+    
+    const newContentHandler = (event) => {
+        setnewContent(event.target.value);
+    };
+
+    const newSetting = (id) =>{
+        const newWorking = data.map((data) => {
+            if(data.id === id) {
+                return {...data, work: newTitle, content: newContent}
+            }
+            return data;
+        });
+
+        localStorage.setItem('todokey', JSON.stringify([...newWorking]));
+    }
+
+    useEffect(() => {
+        newSetting(item.id);
+    },);
     return (
       <div key={item.id} className='todo-box'>
         <div className='todo-text'>
@@ -9,7 +37,15 @@ const Cards = ({item, fnc1, fnc2,fnc3,fnc4, c}) => {
           {item.content}
         </div>
         <div className='update-box hidden'>
-          <UpdateContent item={item}></UpdateContent>
+          <input className='update-title'
+            type='text'
+            value={newTitle}
+            onChange={(event) => newTitleHandler(event)}
+            ></input>
+            <textarea className='update-content'
+            value={newContent}
+            onChange={(event) => newContentHandler(event)}
+            ></textarea>
         </div>
         <div className='buttons'>
           <Button role={'delete'} name={'삭제하기'} fnc={fnc1} para={item}></Button>
